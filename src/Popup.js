5b9b8e912ea1555ce2a8cbbd8064898a08cee905
Nabinsharma123 <nabinsharma16.ns@gmail.com>
2022-04-01 21:30:12 +0530

    commit

--- src/Popup.js
+++ src/Popup.js
@@ -87,7 +87,7 @@ function Popup(props) {
                                 <img className="popup_image" src={baseUrl + props.movie?.backdrop_path} alt={props.movie?.title || props.movie?.name || props.movie?.original_name} />
 
 
-                                <div className="banner_buttons">
+                                <div className="banner_buttons_popup">
                                     <button onClick={playtrailer} className="banner_button">
                                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>
                                         <span >Play</span></button>
